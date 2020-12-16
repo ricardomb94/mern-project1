44c54b438c44af6f759a60ddf58ed2230b35b7a0
ricardomb94 <aymardmb@gmail.com>
2020-12-16 19:22:21 +0100

    fetching user data (in the database) based on authentication and authorization

--- client/main.js
+++ client/main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
+// import { AppContainer } from 'react-hot-loader';
 import App from './App';
 
 render(<App />, document.getElementById('root'));
