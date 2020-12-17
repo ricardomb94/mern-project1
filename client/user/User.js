88f1c3a72c2cd90225844a2d5ca324464f868094
ricardomb94 <aymardmb@gmail.com>
2020-12-17 20:11:32 +0100

    another commit

---
+++ client/user/User.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import { list } from './api-user';
+
+export default function Users() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const abortController = new AbortController();
+    const signal = abortController.signal;
+
+    list(signal).then((data) => {
+      if (data && data.error) {
+        console.log(data.error);
+      } else {
+        setUsers(data);
+      }
+    });
+    return function cleanup() {
+      abortController();
+    };
+  }, []);
+
+  return (
+    <Paper className={classes.root} elevation={4}>
+      <Typography variant="h6" className={classes.title}>
+        All Users
+      </Typography>
+      <List dense>
+        {users.map((item, i) => {
+          return (
+            <Link to={'/user/' + item._id} key={i}>
+              <ListItem button>
+                <ListItemAvatar>
+                  <Avatar>
+                    <Person />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText primary={item.name} />
+                <ListItemSecondaryAction>
+                  <IconButton>
+                    <ArrowForward />
+                  </IconButton>
+                </ListItemSecondaryAction>
+              </ListItem>
+            </Link>
+          );
+        })}
+      </List>
+    </Paper>
+  );
+}
